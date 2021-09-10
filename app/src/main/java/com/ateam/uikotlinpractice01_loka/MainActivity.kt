8761cc46e98a816a8ccd366d2cac66b7f64e8d0e
Loka017 <jessinthespace@gmail.com>
2021-09-10 23:26:35 +0900

    xml IDㅂ여 + 코들린 확장팩(자동완성 지원)

--- app/src/main/java/com/ateam/uikotlinpractice01_loka/MainActivity.kt
+++ app/src/main/java/com/ateam/uikotlinpractice01_loka/MainActivity.kt
@@ -2,12 +2,22 @@ package com.ateam.uikotlinpractice01_loka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        확인버튼이 눌리면? 이벤트 달아주자.
 
+        okBtn.setOnClickListener {
+
+//          입력된 내용? contentEdt의 text 값 조회(get) 변수에 저장해 두자.
+
+
+            val getInputText = contentEdt.text.toString()
+            resultTxt.text = getInputText
+        }
     }
 }
